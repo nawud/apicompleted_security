1959f425f0f606f4f3fdfcccc9ac4dee37aed3bd
RKGoat029-dev <rkgoat029.dev@gmail.com>
2025-01-20 03:05:53 +0100

    Category entity modified + minor clean code changes

--- src/main/java/Product/model/Category.java
+++ src/main/java/Product/model/Category.java
@@ -1,8 +1,26 @@
 package Product.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+@Entity @Table(name = "categories")
 public class Category {
 
-    // id
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank @NotNull
+    @Size(min = 5, max = 30, message = "Category name must be between 5 and 30 characters.")
+    private String name;
 
     // name
     // - not NULL
@@ -10,6 +28,5 @@ public class Category {
 
     // productID (One to Many)
     // - linked to existent products
-
 
 }
