c9745cbc20885d33debd44cc2537a998edba669f
RKGoat029-dev <rkgoat029.dev@gmail.com>
2025-01-27 18:03:18 +0100

    ·Repositories reworked

--- src/main/java/com/example/ecommerce_api/repository/iCategoryRepository.java
+++ src/main/java/com/example/ecommerce_api/repository/iCategoryRepository.java
@@ -2,9 +2,10 @@ package com.example.ecommerce_api.repository;
 
 import com.example.ecommerce_api.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface iCategoryRepository extends JpaRepository<Category, Long> {
+
     Optional<Category> findByName(String name);
+
 }
--- src/main/java/com/example/ecommerce_api/repository/iProductRepository.java
+++ src/main/java/com/example/ecommerce_api/repository/iProductRepository.java
@@ -3,10 +3,11 @@ package com.example.ecommerce_api.repository;
 import com.example.ecommerce_api.model.Category;
 import com.example.ecommerce_api.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 import java.util.Optional;
 
 public interface iProductRepository extends JpaRepository<Product, Long> {
+
     List<Product> findByCategory(Optional<Category> category);
+
 }
