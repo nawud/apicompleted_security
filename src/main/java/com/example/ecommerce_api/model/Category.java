e35d150053052ab0759e93d7aeacd09620da25b4
RKGoat029-dev <rkgoat029.dev@gmail.com>
2025-01-28 13:49:12 +0100

    Category model reworked v2

--- src/main/java/com/example/ecommerce_api/model/Category.java
+++ src/main/java/com/example/ecommerce_api/model/Category.java
@@ -1,6 +1,5 @@
 package com.example.ecommerce_api.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -9,31 +8,27 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import java.util.ArrayList;
 import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter @Setter
-@Entity @Table(name = "categories")
+@Getter
+@Setter
+@Entity
+@Table(name = "categories")
 public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-
-    @Column(name = "name")
+    @Column(name = "name", nullable = false)
     private String name;
 
-    @OneToMany(mappedBy = "category", fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "category", fetch = FetchType.LAZY)
     private List<Product> products = new ArrayList<>();
 
-    public Category (
-            @NotNull @NotBlank(message = "Category is required!")
-            @Size(min = 5, max = 30, message = "Name must be between 5 and 30 characters.")
-            String name
-    ) {}
+    public Category (String name) { this.name = name; }
 
 }
