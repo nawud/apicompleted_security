27223ed54e56633544509294ebf2f3f5ab01b6e3
nawud <duwan17@gmail.com>
2025-03-26 10:29:25 +0100

    added security

---
+++ src/main/java/com/example/apifull_security/securityConfig/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.example.apifull_security.securityConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+
+                        .requestMatchers(HttpMethod.GET, "/api/funko").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/api/funko/{funkoId}").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/api/funko").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/api/funko/{funkoId}").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/funko/{funkoId}").hasRole("ADMIN")
+
+
+                )
+
+                .httpBasic(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+}
