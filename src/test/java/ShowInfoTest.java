5d0cbcba75bcf5c845614c75b40e234ed71818f3
nawud <duwan17@gmail.com>
2025-03-26 20:14:34 +0100

    update

--- src/test/java/ShowInfoTest.java
+++ src/test/java/ShowInfoTest.java
@@ -7,7 +7,6 @@ class ShowInfoTest {
 
     @Test
     public void showInfo_Funko_Test() {
-
         // Preparación
         Funko funko = new Funko();
 
@@ -17,7 +16,32 @@ class ShowInfoTest {
         // Verificación
         String expectedResult = "Funko: Goku, Dragon Ball Character - $ 9.99";
         assertEquals(expectedResult, testFunko);
+    }
 
+    @Test
+    public void showInfo_DifferentFunko_Test() {
+        // Preparación
+        Funko funko = new Funko();
+
+        // Ejecución
+        String testFunko = funko.showInfo("Batman", "Dark Knight Funko", 19.99);
+
+        // Verificación
+        String expectedResult = "Funko: Batman, Dark Knight Funko - $ 19.99";
+        assertEquals(expectedResult, testFunko);
+    }
+
+    @Test
+    public void showInfo_PriceWithDecimal_Test() {
+        // Preparación
+        Funko funko = new Funko();
+
+        // Ejecución
+        String testFunko = funko.showInfo("Superman", "Man of Steel", 15.50);
+
+        // Verificación
+        String expectedResult = "Funko: Superman, Man of Steel - $ 15.5";
+        assertEquals(expectedResult, testFunko);
     }
 
 }
